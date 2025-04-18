934275dd7d2f24b610362c0bf5ab102309f3a6d2
Rice Lin <rice.lin@icloud.com>
2025-04-18 22:42:22 +0800

    新增 AudioRecorder 專案的初始檔案，包括主要應用程式、內容視圖、數據模型、測試檔案及資源設定。新增 .gitignore 檔案以排除不必要的檔案。

---
+++ AudioRecorderTests/AudioRecorderTests.swift
@@ -0,0 +1,17 @@
+//
+//  AudioRecorderTests.swift
+//  AudioRecorderTests
+//
+//  Created by Rice Lin on 4/18/25.
+//
+
+import Testing
+@testable import AudioRecorder
+
+struct AudioRecorderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
